29f2b85d0c3b8a6545c081f9cffec96d0e49d75c
Odim Kingsley <okekings@gmail.com>
2022-10-10 16:38:51 -0700

    A function that prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    n = 0
-    for m in range(x):
+    i = 0
+    k = 0
+    while i < x:
         try:
-            print(my_list[m], end="")
-            n += 1
-        except IndexError:
+            my_list[i]
+        except:
             break
-    print()
-    return n
+        else:
+            k += 1
+            print(my_list[i], end="")
+        i += 1
+    if x != 0:
+        print()
+    return (k)
