fb1ae5e695939b39627964cb9442163c2f8bad1c
Odim Kingsley <okekings@gmail.com>
2022-12-20 14:06:26 -0800

    List all states bojects contain the letter a from the db

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     states = session.query(State).filter(State.name.like('%a%')).all()
 
     for state in states:
-        print("{}: {}".format(state.id, state.nam)
+        print("{}: {}".format(state.id, state.name))
